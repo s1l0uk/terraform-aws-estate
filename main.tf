bd5148dc19b98bf2f769395abce22eb5fb5172fd
Paul Hardy <paul@2ndstudios.com>
2022-08-25 11:07:51 +0100

    Fleshing out more from last night

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-provider "aws" {
-    region = var.region
-}
-
 resource "aws_organizations_organization" "root" {
 }
 
-
+module {
+  source                      = "git::https://www.github.com/s1l0uk/terraform-aws-org-structure"
+  region                      = var.region
+  project_accounts            = var.business_units
+  environments                = var.environments_to_use
+  tags                        = var.tags
+}
