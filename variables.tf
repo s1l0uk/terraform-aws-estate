9adda41e7e38eec2757ab1b64b3a86494eec1056
Paul Hardy <paul@2ndstudios.com>
2022-08-25 11:08:10 +0100

    tracing variables back for access

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "name" {
-  description = "[Optional] The root name for the Project."
-  type        = string
-  default     = "Awesome Project!"
-}
-
 variable "region" {
   description = "[Required] The Region to run operations and build within."
   type        = string
@@ -13,36 +7,37 @@ variable "region" {
 variable "network_cidr_range" {
   description = "[Optional] The Primary Region to run operations and build within."
   type        = string
-  default     = "10.0.0.0/8"
+  default     = null
 }
 
 variable "tags" {
   description = "[Optional] Extra Tags to add to your stack."
   type        = map
   default = {
-    enviroment = "alpha"
+    enviroment = null
   }
 }
 
 variable "enable_private_internet_access" {
   description = "[Optional] Should non-public instance be able to access the Internet via a NAT instance."
   type        = bool
-  default     = true
+  default     = null
 }
 
 variable "tiers" {
   description = "[Optional] How many uniform tiers to create, use 'public' to create a public tier."
-  default     = ["public", "data", "mid"]
   type        = list(string)
+  default     = null
 }
 
 variable "availability_zones" {
   description = "[Optional] A list of Availability zones to operate in."
-  default     = ["a", "b", "c"]
   type        = list(string)
+  default     = null
 }
 
 variable "network_bits" {
   description = "[Optional] The number of network bits to be allocated"
-  default     = 8
+  type        = int
+  default     = null
 }
